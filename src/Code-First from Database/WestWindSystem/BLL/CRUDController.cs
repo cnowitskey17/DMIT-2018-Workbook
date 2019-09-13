47f1848a5db3ec46747e161fe159257bdffcafee
cnowitskey17 <43006964+cnowitskey17@users.noreply.github.com>
2019-09-13 13:33:33 -0600

    Finished adding classes and gridview

---
+++ src/Code-First from Database/WestWindSystem/BLL/CRUDController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+
+namespace WestWindSystem.BLL
+{
+    [DataObject] //for the <ObjectDataSource> COntrol
+    public class CRUDController
+    {
+        #region Products CRUD
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Product> ListProducts()
+        {
+            using(var context = new WestWindContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+        public List<Supplier> ListSupplierss()
+        {
+            using (var context = new WestWindContext())
+            {
+                return context.Suppliers.ToList();
+            }
+        }
+
+        public List<Category> ListCategories()
+        {
+            using (var context = new WestWindContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+
+        public List<Address> ListAddresses()
+        {
+            using (var context = new WestWindContext())
+            {
+                return context.Addresses.ToList();
+            }
+        }
+        //make methods for suppliers, categories and addresses
+        #endregion
+    }
+}
