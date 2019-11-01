d945a2c0fc1d8acc73a6e5d0f22f8572822aa5b3
cnowitskey17 <43006964+cnowitskey17@users.noreply.github.com>
2019-11-01 12:57:40 -0600

    Got user security implemented

--- src/Custom Identity/WebApp/Admin/Security/SecurityDBContextInitializer.cs
+++ src/Custom Identity/WebApp/Admin/Security/SecurityDBContextInitializer.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using DemoSystem.BLL;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
@@ -31,6 +32,46 @@ namespace WebApp.Admin.Security
             #endregion
 
             #region Seed the users
+            // Create an Admin user
+            var adminUser = new ApplicationUser
+            {
+                //ToDO: Move hard-coded values to web.config
+                UserName = "WebAdmin",
+                Email = "Elections2020@Hackers.ru",
+                EmailConfirmed = true
+            };
+
+            //Instantiate the BLL user manager
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+            // - The aplicationUsermanager is a BLL class in this website's App_Start/IdeneityConfig.cs
+            var result = userManager.Create(adminUser, "Pa$$w0rd"); // TODO: Move password
+            if(result.Succeeded)
+            {
+                //Get the Id that was generated for the user we created/added
+                var adminId = userManager.FindByName("WebAdmin").Id;
+                //Add the user to Administratiors role
+                userManager.AddToRole(adminId, "Administrators");
+            }
+
+            //Create the other user accounts for all the people in my Demo database
+            var demoManager = new DemoController();
+            var people = demoManager.ListPeople();
+            foreach(var person in people)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = $"{person.FirstName}.{person.LastName}",
+                    Email = $"{person.FirstName}.{person.LastName}@DemoIsland.com",
+                    EmailConfirmed = true,
+                    PersonId = person.PersonID
+                };
+                result = userManager.Create(user, "Pa$$word1");
+                if(result.Succeeded)
+                {
+                    var userId = userManager.FindByName(user.UserName).Id;
+                    userManager.AddToRole(userId, "Registered Users");
+                }
+            }
             #endregion
             // Note: keep this call to the case class so it can do its seeding work 
             base.Seed(context);
