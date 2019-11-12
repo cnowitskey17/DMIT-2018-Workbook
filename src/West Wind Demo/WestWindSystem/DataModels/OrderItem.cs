13cd37039ca37a1f023d8e23af4f1ed0b4b0aadb
cnowitskey17 <43006964+cnowitskey17@users.noreply.github.com>
2019-11-12 15:28:42 -0700

    added POCOs/DTOs for OrderProcessing

---
+++ src/West Wind Demo/WestWindSystem/DataModels/OrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WestWindSystem.DataModels
+{
+    //Make the rest for what we did in class i guess
+    public class OrderItem
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public short Qty { get; set; }
+        public string QtyPerUnit { get; set; }
+        public short Outstanding { get; set; }
+    }
+}
---
+++ src/West Wind Demo/WestWindSystem/DataModels/ShipperSelection.cs
@@ -0,0 +1,9 @@
+﻿namespace WestWindSystem.DataModels
+{
+    public class ShipperSelection
+    {
+        public int ShipperId { get; set; }
+        public string Shipper { get; set; }
+
+    }
+}
